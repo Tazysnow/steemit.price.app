5cf58165d3ab7a62d753f6063863a656701af8d8
tazysnow <turtaew69@gmail.com>
2018-03-03 17:26:48 +0700

    TRANSLATIONS

---
+++ translations/english/english.ts
@@ -0,0 +1,49 @@
+const ENG_TRANSLATIONS = {
+	your_steem_earnings: 'Your STEEM earnings',
+	type_steem_username: 'Please type in your Steemit.com username:',
+    add_account: 'Add Account',
+    manage_accounts: 'Manage Accounts',
+    your_accounts: 'Your Accounts',
+    account: 'Account',
+
+    contributions_from: 'Contributions from ',
+    note: 'Note',
+    steem_data_loading : "We're collecting your data from the steemit.com database,this may take a while! ",
+    pending_posts: 'Pending Posts:',
+    no_posts_to_display: 'No posts to display',
+    SBD: 'SBD',
+    pending_comments: 'Pending Comments:',
+    no_comments_to_display: 'No comments to display',
+    SBD: 'SBD',
+    REFRESH: 'REFRESH',
+    
+    earnings_of: 'Earnings of ',
+    note: 'Note', 
+    steem_data_loading : "We're collecting your data from the steemit.com database,this may take a while! ",
+    potential_payout: 'Potential Payout',
+    estimated_total_earnings: 'Estimated total Earnings:',
+    estimated_Author_earnings: 'Estimated Author Earnings:',
+    current_exchange_rate : 'current exchange rate',
+    REFRESH: 'REFRESH',
+
+    payout_overview: 'Payout Overview',
+
+    steem_calculator: 'Steem Calculator',
+    potential_post_payout: 'Potential Post Payout:',
+    potential_earnings: 'Potential Earnings',
+    exchange_rate: 'Exchange Rate:',
+    REFRESH: 'REFRESH',
+
+    steem_earnings: 'Steem Earnings',
+    earnings: 'Earnings',
+    contributions: 'Contributions',
+    overview: 'Overview',
+    calculator: 'Calculator',
+    accounts: 'Accounts',
+    add_account: 'Add Account',
+    select_account: 'Select Account',
+    switch_to : 'Switch to ',
+    manage_accounts: 'Manage Accounts',
+
+
+};
---
+++ translations/english/thai.ts
@@ -0,0 +1,49 @@
+const TH_TRANSLATIONS = {
+	your_steem_earnings: 'รายได้ STEEM ของคุณ',
+	type_steem_username: 'โปรดพิมพ์ชื่อผู้ใช้ Steemit.com ของคุณ:',
+    add_account: 'เพิ่มบัญชี',
+    manage_accounts: 'จัดการบัญชี',
+    your_accounts: 'บัญชีของคุณ',
+    account: 'บัญชี',
+
+    contributions_from: 'ผลงานจาก ',
+    note: 'หมายเหตุ',
+    steem_data_loading : "เรากำลังรวบรวมข้อมูลของคุณจากฐานข้อมูลใน steemit.com ซึ่งอาจใช้เวลาสักครู่! ",
+    pending_posts: 'โพสต์ที่รอดำเนินการ:',
+    no_posts_to_display: 'ไม่มีโพสต์ที่จะแสดง',
+    SBD: 'SBD',
+    pending_comments: 'ความเห็นที่รอดำเนินการ:',
+    no_comments_to_display: 'ไม่มีความคิดเห็นที่จะแสดง',
+    SBD: 'SBD',
+    REFRESH: 'รีเฟรช',
+    
+    earnings_of: 'รายได้จาก ',
+    note: 'หมายเหตุ', 
+    steem_data_loading : "เรากำลังรวบรวมข้อมูลของคุณจากฐานข้อมูลใน steemit.com ซึ่งอาจใช้เวลาสักครู่! ",
+    potential_payout: 'การจ่ายเงินที่อาจเกิดขึ้น',
+    estimated_total_earnings: 'รายได้รวมโดยประมาณ:',
+    estimated_Author_earnings: 'รายได้โดยประมาณของผู้เขียน:',
+    current_exchange_rate : 'อัตราการแลกเปลี่ยนปัจจุบัน',
+    REFRESH: 'รีเฟรช',
+
+    payout_overview: 'การจ่ายเงินภาพโดยรวม',
+
+    steem_calculator: 'เครื่องคำนวณ Steem',
+    potential_post_payout: 'การจ่ายเงินโพสต์ที่เป็นไปได้:',
+    potential_earnings: 'รายได้โดยประมาณ',
+    exchange_rate: 'อัตราแลกเปลี่ยน:',
+    REFRESH: 'รีเฟรช',
+
+    steem_earnings: 'เครื่องคำนวณ Steem',
+    earnings: 'รายได้',
+    contributions: 'ผลงาน',
+    overview: 'ภาพรวม',
+    calculator: 'เครื่องคิดเลข',
+    accounts: 'บัญชี',
+    add_account: 'เพิ่มบัญชี',
+    select_account: 'เลือกบัญชี',
+    switch_to : 'เปลี่ยนเป็น ',
+    manage_accounts: 'จัดการบัญชี',
+
+
+};
